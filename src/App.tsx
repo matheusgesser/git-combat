660f8f702d70be521d93eeff9541938a838825e0
MatheusGesser <matheusmvg@hotmail.com>
2023-03-07 13:07:09 -0300

    style: full home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React, { useState } from 'react'
 import './styles/base.css'
+import './styles/home.css'
+import './styles/animations.css'
 
 export default function App() {
   const [showGame, setShowGame] = useState<boolean>(false)
-  const [repos, setRepos] = useState(['s'])
+  const [repos, setRepos] = useState([])
 
   return (
     <main>
       {!showGame && (
         <>
-          <h1>Welcome to GitCombat</h1>
-          <h2>Live star battle of random famous Github repositories</h2>
+          <strong className='floating'>GitCombat</strong>
+          <p>A pair of random GitHub repositories against each other, you guess which one has more stars using your knowledge of programming languages and open source projects.</p>
           {repos.length == 0 ? (
-            <p>Loading...</p>
+            <p className='pulsate-bck'>Loading...</p>
           ) : (
-            <button type='submit' onClick={() => setShowGame(true)}>Play</button>
+            <button type='submit' onClick={() => setShowGame(true)} className='jello-horizontal'>
+              PLAY
+            </button>
           )}
         </>
       )}
