000dd63f1d32a51a9068d2eddd29dc4c395b60b9
MatheusGesser <matheusmvg@hotmail.com>
2023-03-07 17:38:13 -0300

    fix: github token import error

--- src/api/github.ts
+++ src/api/github.ts
@@ -3,7 +3,7 @@ import { Octokit } from '@octokit/core'
 const token = import.meta.env.VITE_GITHUB_TOKEN
 
 const octokit:Octokit = new Octokit({
-  auth: "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",
+  auth: token,
 })
 
 export default octokit
