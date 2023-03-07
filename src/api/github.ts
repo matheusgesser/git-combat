c65722fd3f8f6f205552cfd2c2336cfe42241b71
MatheusGesser <matheusmvg@hotmail.com>
2023-03-07 14:26:32 -0300

    Revert "fix: env import"
    This reverts commit b55bd3b7ea3a743ca6018a6cad1e2764c26a4380.

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,6 +1,6 @@
 import { Octokit } from '@octokit/core'
 
-const token = import.meta.env.GITHUB_TOKEN
+const token = import.meta.env.VITE_GITHUB_TOKEN
 
 const octokit:Octokit = new Octokit({
   auth: token,
