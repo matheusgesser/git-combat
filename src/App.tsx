c8e63281cbe9455b36a0c4b22f6ef915a23e1422
MatheusGesser <matheusmvg@hotmail.com>
2023-03-07 10:50:38 -0300

    feat: add home page basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/base.css'
 
 export default function App() {
+  const [showGame, setShowGame] = useState<boolean>(false)
+  const [repos, setRepos] = useState(['s'])
+
   return (
-    <div>App</div>
+    <main>
+      {!showGame && (
+        <>
+          <h1>Welcome to GitCombat</h1>
+          <h2>Live star battle of random famous Github repositories</h2>
+          {repos.length == 0 ? (
+            <p>Loading...</p>
+          ) : (
+            <button type='submit' onClick={() => setShowGame(true)}>Play</button>
+          )}
+        </>
+      )}
+      {showGame && (
+        // TODO: Create Game Component
+        <>
+          <h1>Game</h1>
+        </>
+      )}
+    </main>
   )
 }
