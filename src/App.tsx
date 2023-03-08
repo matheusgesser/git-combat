bf1de8f5769f37a80fb9d8c2143c2ba68486e154
MatheusGesser <matheusmvg@hotmail.com>
2023-03-08 09:30:20 -0300

    chore: add classname to elements

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,12 @@ export default function App() {
     <main>
       {!showGame && (
         <>
-          <strong>GitCombat</strong>
-          <p>A pair of random GitHub repositories put against each other, guess which one has more stars using your open source projects knowledge</p>
+          <strong className='title'>GitCombat</strong>
+          <p className='description'>A pair of random GitHub repositories put against each other, guess which one has more stars using your open source projects knowledge</p>
           {repos.length == 0 ? (
-            <p className='pulsate-bck'>Loading...</p>
+            <p className='loading pulsate-bck'>Loading...</p>
           ) : (
-            <button type='submit' onClick={() => setShowGame(true)} className='jello-horizontal'>
+            <button type='submit' onClick={() => setShowGame(true)} className='play-btn jello-horizontal'>
               PLAY
             </button>
           )}
