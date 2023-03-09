47cf099ea6779491a98e855634c7248f4f29f35c
MatheusGesser <matheusmvg@hotmail.com>
2023-03-09 10:10:50 -0300

    feat: add stars background

--- src/App.tsx
+++ src/App.tsx
@@ -14,28 +14,35 @@ export default function App() {
   const [repos, setRepos, allRepos] = useRepos(30)
 
   return (
-    <main>
-      {!showGame && (
-        <>
-          <strong className='title'>GitCombat</strong>
-          <p className='description'>A pair of random GitHub repositories put against each other, guess which one has more stars using your open source projects knowledge</p>
-          {repos.length == 0 ? (
-            <p className='loading pulsate-bck'>Loading...</p>
-          ) : (
-            <button type='submit' onClick={() => setShowGame(true)} className='play-btn jello-horizontal'>
-              PLAY
-            </button>
-          )}
-        </>
-      )}
-      {showGame && (
-        <Game
-          repoState={[repos, setRepos]}
-          originalList={allRepos}
-          setShowGame={setShowGame} />
-      )}
-      <audio id='correct-answer-audio' src={correctAnswerAudio} ></audio>
-      <audio id='wrong-answer-audio' src={wrongAnswerAudio} ></audio>
-    </main>
+    <>
+      <div className="background">
+        <div className="stars1"></div>
+        <div className="stars2"></div>
+        <div className="stars3"></div>
+      </div>
+      <main>
+        {!showGame && (
+          <>
+            <strong className='title'>GitCombat</strong>
+            <p className='description'>A pair of random GitHub repositories put against each other, guess which one has more stars using your open source projects knowledge</p>
+            {repos.length == 0 ? (
+              <p className='loading pulsate-bck'>Loading...</p>
+            ) : (
+              <button type='submit' onClick={() => setShowGame(true)} className='play-btn jello-horizontal'>
+                PLAY
+              </button>
+            )}
+          </>
+        )}
+        {showGame && (
+          <Game
+            repoState={[repos, setRepos]}
+            originalList={allRepos}
+            setShowGame={setShowGame} />
+        )}
+        <audio id='correct-answer-audio' src={correctAnswerAudio} ></audio>
+        <audio id='wrong-answer-audio' src={wrongAnswerAudio} ></audio>
+      </main>
+    </>
   )
 }
