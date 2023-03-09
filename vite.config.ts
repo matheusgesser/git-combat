5aa78af29faf95a20bf5f996d7eaddd6bd5c0475
MatheusGesser <matheusmvg@hotmail.com>
2023-03-09 08:51:01 -0300

    fix: remove define global to fix vite-build

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {alias: {'node-fetch':'axios'}},
-  define: {
-    global: {},
-  },
+  resolve: { alias: { 'node-fetch': 'axios' } },
 })
